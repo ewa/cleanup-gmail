a5256e7b8cb3a13ac0f619868325ef374b8d8127
Eric W. Anderson <ewa@users.noreply.github.com>
2023-02-01 22:44:10 -0700

    Add license notice

--- email_reader.py
+++ email_reader.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+# cleanup-gmail: A script to bulk-edit gmail messages to mark older messages as unread
+# Copyright (C) 2023 Eric W. Anderson
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # import the required libraries
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
